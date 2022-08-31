f80692906582f983b0cd5eaad2a6421e6c0c78ce
Xiao <fanxiaoseu@gmail.com>
2022-08-31 19:44:27 +0800

    render with dictionary

--- app.py
+++ app.py
@@ -21,38 +21,49 @@ def getFoundItems():
 
 def getLostItems():
     df = pd.read_csv('static/lost_items.csv')
-    return df.Image.values, df.Description.values
+    return df.to_dict(orient='records')
 	
 @app.route('/')
 def homepage():
-    items, description = getLostItems()
-    items = ['static/img/'+i for i in items]
-    return render_template('index.html',len=len(items), items=items, description=description)
+    records = getLostItems()
+    return render_template('index.html',lost=records, found=records, lost_length=len(records), found_length=len(records))
 
-# @app.route('/', methods=['POST'])
-# def upload_image():
-# 	if 'file' not in request.files:
-# 		flash('No file part')
-# 		return redirect(request.url)
-# 	file = request.files['file']
-# 	if file.filename == '':
-# 		flash('No image selected for uploading')
-# 		return redirect(request.url)
-# 	if file and allowed_file(file.filename):
-# 		filename = secure_filename(file.filename)
-# 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-# 		#print('upload_image filename: ' + filename)
-# 		flash('Image successfully uploaded and displayed below')
-# 		return render_template('upload.html', filename=filename)
-# 	else:
-# 		flash('Allowed image types are -> png, jpg, jpeg, gif')
-# 		return redirect(request.url)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+                request.form['password'] != 'secret':
+            error = 'Invalid credentials'
+        else:
+            flash('You were successfully logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
-# @app.route('/display/<filename>')
-# def display_image(filename):
-# 	#print('display_image filename: ' + filename)
-# 	return redirect(url_for('static', filename='images/' + filename), code=301)
+@app.route('/newlost', methods=['POST'])
+def upload_image():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#print('upload_image filename: ' + filename)
+		flash('Image successfully uploaded and displayed below')
+		return render_template('upload.html', filename=filename)
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='images/' + filename), code=301)
 
 if __name__ == "__main__":
 
-    app.run()
+    app.run(debug=True)
