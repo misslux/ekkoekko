686ffb4eced9488b5e299f5dfa765f8af12033f3
HHX <huixin.hao@siemens.com>
2022-09-01 09:28:41 +0800

    fix: 路径更改，页面跳转

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,flash, request, redirect, url_for
+from flask import Flask, render_template, flash, request, redirect, url_for, session
 import os
 
 # PEOPLE_FOLDER = os.path.join('static', 'people_photo')
@@ -6,27 +6,86 @@ import os
 app = Flask(__name__)
 app.secret_key = "siemens"
 app.config['UPLOAD_FOLDER'] = 'static/images'
+app.secret_key = 'QWERTYUIOP'
 
 from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
+
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 def getFoundItems():
     return
 
+
 def getLostItems():
     return
-	
+
+
 @app.route('/')
 def homepage():
-    items = ['static/img/iPhone.jpg','static/img/rolex.jpg', 
-             'static/img/mi.jpg', 'static/img/latiao.jpg',
-             'static/img/latiao.jpg']
-    description=['Iphone手机','手表', '小米手机', '辣条', '辣条']
-    return render_template('index.html',len=len(items), items=items, description=description)
+    # 改成读文件和文件名避免写死
+    path = "static/img/items/"
+    files = os.listdir(path)
+    items = []
+    description = []
+    for file in files:
+        items.append(path + file)
+        description.append(str(file).split('.')[0])
+
+    # items = ['static/img/iPhone.jpg', 'static/img/rolex.jpg',
+    #          'static/img/mi.jpg', 'static/img/latiao.jpg',
+    #          'static/img/latiao.jpg']
+    # description = ['Iphone手机', '手表', '小米手机', '辣条', '辣条']
+
+    return render_template('index.html', len=len(items), items=items, description=description)
+
+
+@app.route('/index')
+def index():
+    user_info = session.get('user_info')
+    if not user_info:
+        return redirect('/login')
+    return redirect('index')
+
+
+@app.route('/my')
+def my():
+    return render_template('my.html')
+
+
+@app.route('/myinfo')
+def myinfo():
+    return render_template('myinfo.html')
+
+
+@app.route('/surprise')
+def surprise():
+    return render_template('surprise.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    user = request.form.get('form-username')
+    pwd = request.form.get('form-password')
+    if user == 'admin' and pwd == '123':
+        session['user_info'] = user
+        return redirect('/my')
+    else:
+        return render_template('login.html', msg='用户名or密码错误~~')
+
+
+@app.route('/logout')
+def logout():
+    del session['user_info']
+    return redirect('login')
+
+
+
 
 # @app.route('/', methods=['POST'])
 # def upload_image():
